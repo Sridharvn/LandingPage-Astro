a6ad1f29d5d0a770861506952d8b8f08e0234ba9
Sridharvn <sridharvnampoothiripad@gmail.com>
2023-01-26 19:40:28 +0530

    Half finished

---
+++ src/components/OurTeam.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="flex justify-evenly">
+    <div
+      v-for="item in team"
+      :key="item.name"
+      class="p-3 flex flex-col items-center justify-center"
+    >
+      <img v-bind:src="item.profile" alt="" />
+      <div>
+        {{ item.name }}
+      </div>
+      <div class="text-[#ef7216]">
+        {{ item.position }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      team: [
+        {
+          name: "Micheal Wagou",
+          profile: "./../../public/Micheal-Wagou.png",
+          position: "Director",
+        },
+        {
+          name: "Alex Mitchell",
+          profile: "./../../public/Alex-Mitchell.png",
+          position: "Founder",
+        },
+        {
+          name: "Margaret Anderson",
+          profile: "./../../public/Margaret-Anderson.png",
+          position: "Manager",
+        },
+        {
+          name: "John Harris",
+          profile: "./../../public/John-Harris.png",
+          position: "Manager",
+        },
+      ],
+    };
+  },
+  methods: {
+    getImgUrl(pic) {
+      //   return require(pic);
+    },
+  },
+};
+</script>
---
+++ src/components/WhyChooseUs.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex flex-col items-center justify-center p-3 mb-10">
+    <div class="text-[#ef7216] flex justify-center text-center text-xl p-3">
+      WHY CHOOSE US
+    </div>
+    <div class="flex justify-center text-center text-5xl w-1/2 p-3 mb-5">
+      We give you complete control of your process.
+    </div>
+    <div class="flex justify-evenly">
+      <div class="flex items-center w-1/4">
+        <div class="text-[#ef7216] text-9xl">01</div>
+        <div class="flex flex-col">
+          <div class="text-2xl">Request Quotes</div>
+          <div class="text-xl">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit, sed ut labore et
+            dolore magna aliqua.
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center w-1/4">
+        <div class="text-[#ef7216] text-9xl">02</div>
+        <div class="flex flex-col">
+          <div class="text-2xl">Compare Prices</div>
+          <div class="text-xl">
+            Sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua.
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center w-1/4">
+        <div class="text-[#ef7216] text-9xl">03</div>
+        <div class="flex flex-col">
+          <div class="text-2xl">Safely Delivered</div>
+          <div class="text-xl">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusa ntium
+            doloremque.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr class="h-px my-8 bg-[#ef7216] border-0 mx-20" />
+</template>
