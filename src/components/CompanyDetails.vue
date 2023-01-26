c0b35d94053838ff721606a506bbe47920d13d82
Sridharvn <sridharvnampoothiripad@gmail.com>
2023-01-26 17:03:39 +0530

    Details Page finished

---
+++ src/components/CompanyDetails.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex">
+    <div class="flex flex-col w-1/2">
+      <div class="p-6 m-12">
+        <div class="text-[#ef7216] text-2xl">OUR COMPANY</div>
+        <div class="text-4xl">We are the best moving company in the world.</div>
+        <div class="text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua- Sed ut perspiciatis unde omnis iste
+          natus error sit voluptatem accusantium doloremque laudantium. totam rem aperiam,
+          eaque ipsa quae ab illo inventore veritatis et quasi natus error sit voluptatem
+          architecto beatae vitae dicta sunt explicabo.
+        </div>
+      </div>
+      <div class="flex justify-center divide-x-2">
+        <div class="flex items-center p-2">
+          <img src="./../../public/smiley-icon.png" alt="" class="h-16 w-16" />
+          <div class="flex flex-col p-3">
+            <div class="text-4xl">480+</div>
+            <div class="text-xl">Happy Customers</div>
+          </div>
+        </div>
+        <div class="flex items-center p-2">
+          <img src="./../../public/package-icon.png" alt="" class="h-16 w-16" />
+          <div class="flex flex-col p-3">
+            <div class="text-4xl">65+</div>
+            <div class="text-xl">Cranes and Trailers</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-1/2 h-max">
+      <img src="./../../public/details-image.png" alt="" />
+    </div>
+  </div>
+  <hr class="h-px my-8 bg-[#ef7216] border-0 mx-20" />
+</template>
--- src/components/HeroImage.vue
+++ src/components/HeroImage.vue
@@ -4,7 +4,7 @@
       src="./../../public/pexels-tom-fisk-2226458.jpg"
       class="h-[85vh] w-full object-cover"
     />
-    <div class="absolute text-2xl text-blue-300 bottom-0 left-5">
+    <div class="absolute text-blue-300 bottom-0 left-5">
       <div class="bg-[#ef721695] w-[400px] text-white flex flex-col p-6 ml-12">
         <div class="p-3 text-3xl">We Offer client wide range of Manlift</div>
         <div class="p-3 text-xl">
@@ -14,7 +14,7 @@
         <div class="bg-white text-[#ef7216] w-1/3 p-3 flex justify-center">Read More</div>
       </div>
     </div>
-    <div class="absolute text-2xl bottom-0 right-5">
+    <div class="absolute bottom-0 right-5">
       <div class="flex">
         <div class="bg-white text-3xl p-10">&#60;</div>
         <div class="bg-[#ef7216] text-white text-3xl p-10">&#62;</div>
